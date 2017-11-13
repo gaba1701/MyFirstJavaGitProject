c6c1668922ae551aed92f0a505eea09594d49fcf
Anna Barysheva <gannabarysheva@gmail.com>
2017-11-13 13:44:25 +0100

    This is my third commit. I am finished with this project

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Random ran = new Random();
+		int value = ran.nextInt(21);
+		if (value % 2==0) {
+			System.out.println("this number is even");}
+			else {
+				System.out.println("This number is odd");
+			}
+		}
 
 	}
 
-}
+
